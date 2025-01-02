293eb4cbf5fab36aeef6ea3d5558072e5396a7ea
aarock1234 <arockplaysmc@gmail.com>
2025-01-01 21:19:46 -0800

    ua

--- src/classes/login.ts
+++ src/classes/login.ts
@@ -24,7 +24,7 @@ export class LoginAndComplete extends EventEmitter {
 		'sec-ch-ua-mobile': '?0',
 		'upgrade-insecure-requests': '1',
 		'user-agent':
-			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 		accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 		'sec-fetch-site': 'same-origin',
 		'sec-fetch-mode': 'navigate',
@@ -48,7 +48,7 @@ export class LoginAndComplete extends EventEmitter {
 				'sec-ch-ua-mobile': '?0',
 				'upgrade-insecure-requests': '1',
 				'user-agent':
-					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 				accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 				'sec-fetch-site': 'same-origin',
 				'sec-fetch-mode': 'navigate',
--- src/classes/quiz.ts
+++ src/classes/quiz.ts
@@ -42,7 +42,7 @@ export class Quiz extends EventEmitter {
 		origin: 'https://www.wizard101.com',
 		'content-type': 'application/x-www-form-urlencoded',
 		'user-agent':
-			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 		accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 		'sec-fetch-site': 'same-origin',
 		'sec-fetch-mode': 'navigate',
