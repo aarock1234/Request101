735a4666d1f48a3e5f0d14bee9cbd428a7301270
aarock1234 <arockplaysmc@gmail.com>
2025-03-04 15:37:34 -0800

    ua

--- src/classes/quiz.ts
+++ src/classes/quiz.ts
@@ -36,13 +36,13 @@ export class Quiz extends EventEmitter {
 	postHeaders = {
 		host: 'www.wizard101.com',
 		'cache-control': 'max-age=0',
-		'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+		'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
 		'sec-ch-ua-mobile': '?0',
 		'upgrade-insecure-requests': '1',
 		origin: 'https://www.wizard101.com',
 		'content-type': 'application/x-www-form-urlencoded',
 		'user-agent':
-			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+			'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
 		accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 		'sec-fetch-site': 'same-origin',
 		'sec-fetch-mode': 'navigate',
